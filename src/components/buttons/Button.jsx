3e4e1d60f1364c92ff70c236f2892cb8e6bf44c6
MaikeraW <wagner.maiconhenrique@gmail.com>
2026-05-08 11:24:22 -0300

    css modules classes

---
+++ src/components/buttons/Button.jsx
@@ -0,0 +1,14 @@
+import styles from './Button.module.css'
+
+
+function Button(){
+
+    return (
+        <>
+        <h1 className={styles.title}>AQUI VAI UM TESTE BOLADO</h1>
+        <button className={styles.btn}>First Button</button>
+        </>
+    )
+
+}
+export default Button
