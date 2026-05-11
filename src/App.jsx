5d42b6f1d14d5a34c7097e7770163c8b4a452957
MaikeraW <wagner.maiconhenrique@gmail.com>
2026-05-11 12:38:54 -0300

    useEffect class finalizada

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,22 @@ import Button from "./components/buttons/Button"
 function App() {
 
   const [contador, setContador] = useState("Desligada")
-  useEffect(() => {alert("A luz está: " + contador)}, [contador]) 
+  const [btnName, setBtnName] = useState("Ligar")
+
+  function btnPressed(){
+    setContador(contador === "Desligada" ? "Ligada" : "Desligada" )
+    setBtnName(btnName === "Ligar" ? "Desligar" : "Ligar")
+
+  }
+  useEffect(() => {}, [contador]) 
   //Executa a função quando o elemente é renderizado e a depender do array de dependências. Se estiver vazio, executa sempre que renderizar o componente.
   //Se tiver array de dependencias, executa sempre que o valor dos elementos do array forem alterados
   //Se a função parametro tiver um Return ( () => {} ) - esse return será executado quando o elemento foi removido da tela
 
   return (
     <>
-      <Button btnName="Incrementar" onClick={() => setContador(contador === "Desligada" ? "Ligada" : "Desligada" )}/>
+      <Button btnName={btnName} onClick={btnPressed}/>
+      <h1>A luz está {contador}</h1>
     </>
   )
 }
